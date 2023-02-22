bb95003f4c5a87a8652233d0709dbf77601dc787
Yaroslaw-aw <manumission@mail.ru>
2023-02-23 01:53:12 +0300

    Task 25 3% ready

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿namespace acmp
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Formatters;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace acmp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = new Random().Next(0, int.MaxValue);
+            int b = new Random().Next(0, int.MaxValue);
+
+            if (a > b)
+            {
+                Console.WriteLine(">");
+            }
+            if (b > a)
+            {
+                Console.WriteLine("<");
+            }
+            if (a == b)
+            {
+                Console.WriteLine("="); 
+            }
         }
     }
 }
