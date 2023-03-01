1f0b1b5260a893dac8b0f1a88e88184b9a6a0282
Yaroslaw-aw <manumission@mail.ru>
2023-03-01 21:34:46 +0300

    Task21 ready

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿namespace acmp
+﻿using System;
+namespace acmp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string ABC = Console.ReadLine();
+
+            string[] numbers = ABC.Split();
+
+            uint A = (uint)Convert.ToInt32(numbers[0]);
+            uint B = (uint)Convert.ToInt32(numbers[1]);
+            uint C = (uint)Convert.ToInt32(numbers[2]);
+
+            uint max = A;
+            uint min = B;
+
+            if (B > A)
+            {
+                max = B;
+                min = A;
+            }
+
+            if (C > max)
+                max = C;
+            if (C < min)
+                min = C;
+            Console.WriteLine(max - min);
+
         }
     }
 }
