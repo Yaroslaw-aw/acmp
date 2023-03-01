3bcecf90558c23356930d304133726e6291e5f7b
Yaroslaw-aw <manumission@mail.ru>
2023-03-01 22:16:58 +0300

    Task 8 5% ready

--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
-﻿namespace acmp
+﻿using System;
+
+namespace acmp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string ABC = Console.ReadLine();
+
+            string[] numbers = ABC.Split();
+
+            int A = Convert.ToInt32(numbers[0]);
+            int B = Convert.ToInt32(numbers[1]);
+            int C = Convert.ToInt32(numbers[2]);
+
+            bool yes = A * B == C;
+
+            bool ok = A <= 100 && B <= 100;
+
+            if (!ok)
+            {
+                Console.WriteLine("NO");
+            }
+
+            if (yes)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }           
+
         }
     }
 }
